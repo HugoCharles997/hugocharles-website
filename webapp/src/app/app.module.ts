76ac396bbe7eb8265f80837612d85a6d21a0862d
HugoCharles997 <hugo.charles.pro997@gmail.com>
2023-12-18 21:31:04 +0100

    add variables, bootstrap and some tests

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { MainLayoutModule } from './layout/main-layout/main-layout.module';
 
 @NgModule({
   declarations: [AppComponent, MainLayoutComponent],
-  imports: [BrowserModule, AppRoutingModule, MainLayoutModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, MainLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
