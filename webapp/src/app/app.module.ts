6d21799404b77d4574211f38078d8e95f0c91327
HugoCharles997 <hugo.charles.pro997@gmail.com>
2023-12-18 22:30:39 +0100

    setup material and imports

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -5,10 +5,20 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { HeaderComponent } from './layout/main-layout/header/header.component';
+import { FooterComponent } from './layout/main-layout/footer/footer.component';
+import { MainContentComponent } from './layout/main-layout/main-content/main-content.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent, MainLayoutComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContentComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
