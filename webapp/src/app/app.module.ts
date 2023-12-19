081604156c2f927b22ed1ce95eb780fccfa9f1af
HugoCharles997 <hugo.charles.pro997@gmail.com>
2023-12-19 16:11:25 +0100

    create sidenav and toggle menu

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -7,8 +7,11 @@ import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { HeaderComponent } from './layout/main-layout/header/header.component';
 import { FooterComponent } from './layout/main-layout/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainContentComponent } from './layout/main-layout/main-content/main-content.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,9 +19,17 @@ import { MatButtonModule } from '@angular/material/button';
     MainLayoutComponent,
     HeaderComponent,
     FooterComponent,
+    NavbarComponent,
     MainContentComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, MatButtonModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    MatButtonModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- webapp/src/app/layout/main-layout/header/header.component.ts
+++ webapp/src/app/layout/main-layout/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  showMenu = false;
 
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
 }
