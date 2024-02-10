65c0a23fd534fa065eb68710bbc28feed165d9c9
HugoCharles997 <hugo.charles.pro997@gmail.com>
2024-02-10 11:19:22 +0100

    animate footer icons

--- webapp/src/app/layout/main-layout/footer/footer.component.ts
+++ webapp/src/app/layout/main-layout/footer/footer.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit {
+  ngOnInit(): void {
+    this.animSocialMediaIcons();
+  }
+
+  timeout: boolean = false;
+
+  private animSocialMediaIcons(): void {
+    gsap.from('.twitter', {
+      duration: 1.5,
+      ease: 'expo.out',
+      x: -230,
+      scale: 0.5,
+    });
+
+    gsap.from('.github', {
+      duration: 1.5,
+      ease: 'expo.out',
+      x: -230,
+      scale: 0.5,
+    });
+
+    gsap.from('.linkedin', {
+      duration: 1.5,
+      ease: 'expo.out',
+      x: -230,
+      scale: 0.5,
+    });
+  }
+}
